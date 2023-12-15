801c24b2d8cfb37f7f1a9a1c46b18dbaa07b0cc4
Laith Alferjani <laithahmad70@gmail.com>
2023-12-15 19:56:07 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // fontFamily: {'Almarai': ['Almarai', 'sans-serif']}, // Add Almarai font
+      fontFamily: {'Alexandria': ['Alexandria', 'sans-serif']}, // Add Cairo font
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require('flowbite/plugin'),
+  ],
+}
